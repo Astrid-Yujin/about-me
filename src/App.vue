47fa565b4a0c6b4b4017122efa75c475f40b8222
Yujin Wang <astrid.wangyujin@gmail.com>
2021-08-11 21:08:19 +0200

    Add: home component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
+    <main class="main-content">
       <router-view></router-view>
     </main>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+  export default {
+    name: 'app',
+  };
 </script>
 
 <style>
@@ -25,30 +21,9 @@ body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
 }
 
-main {
+.main-content {
   text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="home">
+      <div class="row">
+        <div class="content-wrapper">
+          <h1>{{ headline }}</h1>
+          <h2>{{ subHeadline }}</h2>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  props: {
+    headline: {
+      type: String,
+      default: 'Yujin Wang',
+    },
+    subHeadline: {
+      type: String,
+      default: 'A happy frontend developer :)',
+    },
+  },
+};
+</script>
+
+<style>
+.row {
+  position: relative;
+  width: 100%;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  margin: 0 auto;
+}
+
+@media only screen and (min-width: 1600px){
+  .row {
+    max-width: 1200px;
+  }
+}
+
+.home {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  min-height: 40rem;
+  background-color: black;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+}
+
+@keyframes rotation {
+  from {
+    transform: rotate(-30deg);
+  }
+  to {
+    transform: rotate(30deg);
+  }
+}
+
+.home::before {
+  content: "";
+  display: block;
+  position: absolute;
+  height: 300vh;
+  width: 300vw;
+  top: -100vh;
+  right: -5vw;
+  background-image: url('../assets/svg/starwheel.svg');
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: center center;
+  animation: rotation 60s linear infinite;
+}
+
+.home .content-wrapper {
+  width: 50%;
+}
+</style>
